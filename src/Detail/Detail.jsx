8b218ea17421d2b3c84aa65447ac69cac1f879bc
RonETran <rontran23@gmail.com>
2023-04-26 19:39:26 +0700

    'fix'

--- src/Detail/Detail.jsx
+++ src/Detail/Detail.jsx
@@ -4,22 +4,22 @@ export default class Detail extends Component {
   render() {
     let {detail} = this.props;
     return (
-        <div class="modal fade" id="modal-detail" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Chi tiết</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div className="modal fade" id="modal-detail" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Chi tiết</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body d-flex align-items-center">
+            <div className="modal-body d-flex align-items-center">
               <img src={detail.image} width={250} alt="" className='me-4'/>
               <div>
                 <h3 className='fs-32'>{detail.name}</h3>
                 <p className='fs-20'>{detail.description}</p>
               </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
             </div>
           </div>
         </div>
